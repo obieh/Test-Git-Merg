7ed556846c1b7364d3dbcdcec3e4caef45e0c00b
obieh <o_amadic@yahoo.com>
2025-04-17 23:52:01 +0100

    updated first print

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ temp = x
 x = y
 y = temp
 
-print('The value of x after swapping: {}'.format(x))
+print(f"The value of x after swapping is {x}")
 print('The value of y after swapping: {}'.format(y))
