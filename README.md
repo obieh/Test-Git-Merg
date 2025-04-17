16f2ea107d564559eff880e1c7ee5401eb84ed22
obieh <o_amadic@yahoo.com>
2025-04-17 23:43:38 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Test-Git-Merg
+# Test-Git-Merg.
+## This project will demonstrate my understanding of merging two different branches from two developers working on the same project.
+## The first developer will update the first print statement to f string.
+## The second developer will update the second print statement.
+## Then their branches will be merged to main in github.
---
+++ main.py
@@ -0,0 +1,17 @@
+# Python program to swap two variables
+
+x = 5
+y = 10
+
+# To take inputs from the user
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
+#We will use f' string to test merging on two branches
+
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
